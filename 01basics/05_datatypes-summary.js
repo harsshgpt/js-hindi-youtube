78ff942edbc56b125d5530b7dad2f9b43b93a012
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-09-04 15:48:09 +0000

    data types summary

---
+++ 01basics/05_datatypes-summary.js
@@ -0,0 +1,10 @@
+// there are two kind of data types (Primitive & non primitive)
+
+//Primitive
+     // 7 category : string , Number , boolean , null  , undefined , Symbol , BigInt
+
+const score = false
+const id = Symbol('123')
+
+//Refrece Type  (Non - primitive)
+    // array , objects , functions
