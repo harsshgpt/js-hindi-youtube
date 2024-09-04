324786a261c14bab211e08a79c226606c90f6ed3
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-09-04 13:39:25 +0000

    comparison which we have to avoid

---
+++ 01basics/04_comparison.js
@@ -0,0 +1,15 @@
+//Avoide these kind of comparison
+
+
+
+// // console.log( null >0)
+// // console.log( null ==0)
+// // console.log( null >= 0)
+
+// console.log(undefined<0)
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined!=0)
+
+// // === it check data type also\
+// console.log("2" == 0)
