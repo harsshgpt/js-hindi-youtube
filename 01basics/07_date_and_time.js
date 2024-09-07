6ec65dc8835ce83cf13cf11ede97a6cf1e399b27
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-09-07 20:10:20 +0000

    new methodes in terms of date an time

--- 01basics/07_date_and_time.js
+++ 01basics/07_date_and_time.js
@@ -1,22 +1,29 @@
-// let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate); // the type of date is OBJECT
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate); // the type of date is OBJECT
 
-// //+++++++++++++++++++++++++++++++ these are the outputs ++++++++++++++++++++++++
-// // Sat Sep 07 2024 19:50:38 GMT+0000 (Coordinated Universal Time)
-// // Sat Sep 07 2024
-// // 2024-09-07T19:50:38.424Z
-// // 2024-09-07T19:50:38.424Z
-// // 9/7/2024, 7:50:38 PM
+//+++++++++++++++++++++++++++++++ these are the outputs ++++++++++++++++++++++++
+// Sat Sep 07 2024 19:50:38 GMT+0000 (Coordinated Universal Time)
+// Sat Sep 07 2024
+// 2024-09-07T19:50:38.424Z
+// 2024-09-07T19:50:38.424Z
+// 9/7/2024, 7:50:38 PM
 
-// let myCreatedDate = new Date (2024,11,12); //moths start in js from 0-11
-// console.log(myCreatedDate.toDateString());
+let myCreatedDate = new Date (2024,11,12); //moths start in js from 0-11
+console.log(myCreatedDate.toDateString());
 
 //in this methode we are gonna create a date an d time both methode by which we are gonna make or diffrentiate date and time via (toLocalString())
 
-let mydateandtime = new Date (2025,9,14,15,45,10);
+// let mydateandtime = new Date ("2023-01-15");
+let mydateandtime = new Date ("01-14-2023");
 console.log(mydateandtime.toLocaleString())
+
+let myTimeStamp = Date.now();
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate);
