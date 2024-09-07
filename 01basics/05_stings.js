c6b357728c4e0bb6504f2fe40a25861eeed745ce
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-09-07 15:05:10 +0000

    .

---
+++ 01basics/05_stings.js
@@ -0,0 +1,7 @@
+const name = "harsh"
+const repocount = 46
+// console.log(name + repocount + "value");
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+const gamename = new String('hahrsg')
+console.log(gamename.toUpperCase())
