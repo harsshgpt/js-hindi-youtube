15897a228ce0a0c88166057f2203e488f180587c
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-09-07 18:23:49 +0000

    number and math all methode

---
+++ 01basics/06_numbers_and_math.js
@@ -0,0 +1,32 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(2000.21231)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const othernumber = 534.565564;
+
+
+// console.log(othernumber.toPrecision(3))
+// const hund = 100000
+// console.log(hund.toLocaleString('en-IN'))
+
+//++++++++++++++++++++ MATH's +++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.6))
+// console.log(Math.min(4,45,343,223))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+const min = 10;
+const max = 20 ;
+
+console.log(Math.random() * (max-min + 1))
