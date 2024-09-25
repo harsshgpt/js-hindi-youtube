43c5b177b898fe032cd6213c96d349e42ce4c0cc
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-09-25 13:39:47 +0000

    obj2

---
+++ 02basics/04_objects.js
@@ -0,0 +1,20 @@
+const tinderUser = new Object();
+const tinderUser2 ={};
+
+tinderUser.id = "45464"
+tinderUser.name = "harsh"
+tinderUser.isLoggedin = false 
+
+console.log(tinderUser)
+console.log(tinderUser2)
+
+const regularuser = {
+    email : "harsh guaoshasa",
+    fullname :{
+        userfullname:{
+            firstname : harsh,
+
+        }
+    }
+}
+console.log(regularuser.fullname)
