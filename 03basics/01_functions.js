87d50b047f4689eb630f6d8ac38cfa501fa6880f
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-12-25 15:16:24 +0000

    added function

---
+++ 03basics/01_functions.js
@@ -0,0 +1,13 @@
+function sum(num1,num2){
+    let res = num1+num2
+    return res
+    //to save lines we can use ++++++++++ return num1+num2 it execute same operation in single line 
+}
+ let result = sum(4,6)
+//  console.log(result)
+
+function justloggedin(username){
+    return `${username} just logged in`
+}
+let name = justloggedin("harsh")
+console.log(name)
