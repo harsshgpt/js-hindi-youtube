60758d33cb7af2989d6b985b6385eae8b31a3b88
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-12-27 16:46:23 +0000

    hb

---
+++ 04_controleflow/switch.js
@@ -0,0 +1,13 @@
+const month = 12;
+
+switch(month){
+    case(month<10):
+    console.log("its under 10")
+    break;
+    case(month == 12):
+    console.log("its december ");
+    default:
+        console.log("enter the value");
+        break;
+    
+}
---
+++ 04_controleflow/truthy.js
@@ -0,0 +1,8 @@
+// the truthy value are those value which we assume true 
+//falsy value when the value is vacant
+
+const name = "Harsh"
+if (name){
+    console.log(`your name is ${name}`);
+    
+}
