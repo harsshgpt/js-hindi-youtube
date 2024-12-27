f02d2c8c1599b110f7b452d394a6ec2ecb42a1cf
Harsh Gupta <102489089+harsshgpt@users.noreply.github.com>
2024-12-27 17:24:39 +0000

    controleflow fullly learned

--- 04_controleflow/one.js
+++ 04_controleflow/one.js
@@ -1,3 +1,4 @@
+//!== used to check if both time are not same 
 // Diffrence between = and == and === is = is assigning opertaor == is comapritor operator it check whether both value are same or not === it works like == but it also check the type of littral which we are providing to it 
 if(2 != 2){
     console.log("hi i am harsh") ;    // where we write code in object or in braces is called explicit context 
@@ -27,7 +28,7 @@ if (balance > 2000){
 let name = 20
  name = 2
     console.log(name);
-    
+
 //&& is and if both true then true except false 
 // || or if one of two context is saying true then true like 1 0 1, only if 0 0 then it will give 0 value 
 
--- 04_controleflow/truthy.js
+++ 04_controleflow/truthy.js
@@ -1,3 +1,4 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // the truthy value are those value which we assume true 
 //falsy value when the value is vacant
 
@@ -6,3 +7,40 @@ if (name){
     console.log(`your name is ${name}`);
     
 }
+
+//falsy numbers
+
+// 0 , -0,"",null,undefined , BigInt 0n ,NaN , false
+
+//truthy values
+// "0" , " " ,'false' , [] ,{},function(){}
+
+
+const object = {}
+
+if(Object.keys(object).length === 0 ){   // to check object is emppety oor not
+    console.log("object is empety");
+    
+}
+
+const arr =  []
+
+if (arr.length === 0 ){   // this functuon is used to check whether array is emppety or not 
+    console.log("array is empety");
+    
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//NULLISH COALESCING OPERATOR  ??  to avoid null and undefined case afte moving from it any first value will found and printed or passes
+
+let val;
+val = null ?? 10  // here it will give 10 (null and undefined not considered)
+val =undefined ??10??20 //here also 10  will pass because it is at first and after null 
+console.log(val)
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//terniary Operator
+//condition ? true:false
+
+const amount = 1000
+
+amount >900 ? console.log("too much price"):console.log("too less price")
